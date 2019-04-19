8e6098ed5a8240aed76ca3db361182d729368ce4
gbrusman <gabrielbrusman@gmail.com>
2019-04-19 11:56:40 -0700

    Fixed centering and resizing issue. Still need time controls and "next" button.

--- Application.py
+++ Application.py
@@ -10,17 +10,34 @@ class Window(Frame):
 
     def init_window(self):
         self.master.title = "ScheduleBot a.01"
-        label_frame = Frame(root)
-        label_frame.columnconfigure(0, weight=1)
-        label = Label(label_frame, text="Please fill out the information in the form below")
-        label.grid(column=0, row=0)
-        label_frame.grid(sticky=N+S+E+W, row=0, column=0)
+        prompt_frame = Frame(root)
+        prompt_frame.grid_columnconfigure(0, weight=1)
+        prompt_frame.grid(sticky=N + S + E + W, row=0, columnspan=2)
+        prompt = Label(prompt_frame, text="Please fill out the information in the form below")
+        prompt.grid(column=0, row=0, columnspan=2, in_=prompt_frame)
+
+
+        major_frame = Frame(root)
+        major_frame.grid(column=0,row=5)
+        major_label = Label(major_frame, text="Major: ")
+        major_label.grid(column=0, row=0, in_=major_frame)
         major_choices = ["AB Mathematics: Plan 1 - General Mathematics", "AB Mathematics: Plan 2 - Secondary Teaching", "BS Mathematics: Plan 1 - General Mathematics", "BS Mathematics: Plan 2 - Secondary Teaching", "Applied Mathematics", "Mathematical Analytics and Operations Research", "Mathematical and Scientific Computation - Computational and Mathematical Biology Emphasis", "Mathematical and Scientific Computation - Computational and Mathematics Emphasis"]
-        major_select_box = Combobox(root, values=major_choices, state="readonly", width=45)  # need to talk to IT people about what libraries are okay to import
-        major_select_box.grid(row=5, column=0)
+        major_select_box = Combobox(major_frame, values=major_choices, state="readonly", width=45)  # need to talk to IT people about what libraries are okay to import
+
+        major_select_box.grid(row=0, column=1, in_=major_frame, sticky=E+W, padx=5)
+
+        cur_quarter_label = Label(major_frame, text="Current Quarter: ")
+        cur_quarter_label.grid(column=0, row=1, in_=major_frame, pady=20)
+        cur_quarter_choices = ["Fall", "Winter", "Spring"]
+        cur_quarter_select_box=Combobox(major_frame, values=cur_quarter_choices, state="readonly", width=25)
+        cur_quarter_select_box.grid(row=1, column=1, in_=major_frame, sticky=E+W, padx=5)
+
+
+
 
         col_count, row_count = root.grid_size()
-        root.columnconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+        #root.rowconfigure(0, weight=1)
         for row in range(row_count):
             root.grid_rowconfigure(row, minsize=20)
 
