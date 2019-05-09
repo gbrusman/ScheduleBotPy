6f29353b58714b75e32a585a8b0858c621a1acc1
gbrusman <gabrielbrusman@gmail.com>
2019-05-09 11:55:40 -0700

    Made it so user can't continute w/o picking 2Q sequence

--- AppliedSeriesChoicePage.py
+++ AppliedSeriesChoicePage.py
@@ -16,6 +16,7 @@ class AppliedSeriesChoicePage(tk.Frame):
         self.grid_columnconfigure(0, weight=1)
 
         self.button_var = tk.IntVar()
+        self.button_var.set(0)
         radio_button_frame = Frame(self)
         radio_button_frame.grid(row=5, sticky="nsew")
         phy_button = Radiobutton(radio_button_frame, text="PHY 9A,B", variable=self.button_var, value=1).grid(in_=radio_button_frame, row=0, sticky="nsew", pady=5)
@@ -31,15 +32,22 @@ class AppliedSeriesChoicePage(tk.Frame):
         next_button = Button(button_frame, text="Next", command=lambda: self.goto_course_select())  # also need to change courseselect to go back to here if student is LAMA
         next_button.grid(row=25, column=1, padx=5, pady=10, sticky="se", in_=button_frame)
 
+        col_count, row_count = self.grid_size() # sizing the window/spacing the rows
+        for i in range(row_count):
+            self.grid_rowconfigure(i, minsize=1)
+
+        self.grid_rowconfigure(0, minsize=50)
+
     def goto_course_select(self):
-        self.get_info_from_rbuttons()
-        self.controller.show_frame("CourseSelectPage")
+        if self.button_var.get() != 0:
+            self.get_info_from_rbuttons()
+            self.controller.show_frame("CourseSelectPage")
 
     def get_info_from_rbuttons(self):  # This is a horrible way of doing this. Should use classes_by_name and get from dict instead
         if self.button_var.get() == 1:
             for course in self.controller.classes_offered:
                 if course.name == "PHY9A" or course.name == "PHY9B":
-                   course.required["LAMA"] = True
+                    course.required["LAMA"] = True
         elif self.button_var.get() == 2:
             for course in self.controller.classes_offered:
                 if course.name == "BIS2A" or course.name == "BIS2B":
