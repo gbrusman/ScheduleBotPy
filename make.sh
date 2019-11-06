b6e5adb29e8ea413bf8a03e1b42a8ab608388e2b
gbrusman <gabrielbrusman@gmail.com>
2019-11-06 11:09:32 -0800

    Updated make.sh so that it no longer includes "_TEST" in the name of the executable or directory.

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 scripts="AcademicTime.py AppliedSeriesChoicePage.py Course.py CourseSelectPage.py InterestSelectPage.py MajorSelectPage.py MultiPageApp.py ScheduleBlock.py ScheduleDisplayPage.py Schedule.py Student.py"
 
-name="ScheduleBotLINUX_TEST"
+name="ScheduleBotLINUX"
 
 options="-n ${name} " 
 
