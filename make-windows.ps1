603b85085b69a23ee1ef8b9cb69aa81ae7aec2a7
gbrusman <gabrielbrusman@gmail.com>
2019-11-06 10:52:06 -0800

    added script to run pyinstaller from powershell. Not tested yet. Don't think we need any sort of staticx on windows.

---
+++ make-windows.ps1
@@ -0,0 +1,15 @@
+﻿ param (
+    [switch]$f = $false
+ )
+
+
+$scripts = "AcademicTime.py AppliedSeriesChoicePage.py Course.py CourseSelectPage.py InterestSelectPage.py MajorSelectPage.py MultiPageApp.py ScheduleBlock.py ScheduleDispla
+yPage.py Schedule.py Student.py"
+
+$options = "-n ScheduleBotWINDOWS"
+
+if($f) {
+    $options += "--onefile --windowed"
+}
+
+pyinstaller $options $scripts
