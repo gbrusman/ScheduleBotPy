87713b8f24ab50c90b092938f9cd154fa604e4ce
gbrusman <gabrielbrusman@gmail.com>
2020-02-06 13:36:41 -0800

    Wrote script to parse prerequisites as text, so no need for 3 tables. Just have one columm written with english and parentheses that details the prereqs and parse that.

---
+++ prerequisites_test2.py
@@ -0,0 +1,118 @@
+import psycopg2
+from Student import Student
+from Course import Course
+
+def convert_interest_format(interest):
+        interest = interest.replace("_", " ").title()
+        return interest
+
+def get_info_from_database(classes_offered, classes_by_name):
+        conn = psycopg2.connect(host="localhost", database="Math Courses", user="postgres", password="MN~D=bp~+WR2/ppy")
+        cur = conn.cursor()
+        # Fill in data from 'courses' table and put it in the dict.
+        cur.execute("SELECT * FROM courses ORDER BY display_index ASC;")
+        for record in cur:
+            new_course = Course(name=record[0], after=record[1], enrichment_a=record[2], enrichment_b=record[3],
+                                enrichment=record[4], approved_ud_nonmath=record[5], biology_requirement=record[6],
+                                computation_requirement=record[7])
+            classes_by_name[record[0]] = new_course
+
+        # Get names of all of the majors currently offered
+        cur.execute(
+            "SELECT column_name FROM information_schema.columns WHERE table_name = 'required' AND column_name != 'name';")
+        majors = []
+        for record in cur:
+            majors.append(record[0])
+
+        # Fill in data about major requirements from 'required' table.
+        cur.execute("SELECT * FROM required;")
+        for record in cur:
+            required_dict = {}
+            for i in range(1, 9):
+                required_dict[majors[i - 1]] = record[i]
+            classes_by_name[record[0]].required = required_dict
+
+        # Get names of all of the majors currently offered
+        cur.execute(
+            "SELECT column_name FROM information_schema.columns WHERE table_name = 'course_offerings' AND column_name != 'name';")
+        quarters = []
+        for record in cur:
+            quarters.append(record[0])
+
+        # Fill in data about course offerings from 'course_offerings' table.
+        cur.execute("SELECT * FROM course_offerings;")
+        for record in cur:
+            quarters_offered = []
+            for i in range(1, 6):
+                if (record[i]):
+                    quarters_offered.append(quarters[i - 1])
+            classes_by_name[record[0]].quarters_offered = quarters_offered
+            classes_by_name[record[0]].offered_pattern = record[6]
+
+        # Get names of all of the interests currently supported
+        cur.execute(
+            "SELECT column_name FROM information_schema.columns WHERE table_name = 'interests' AND column_name != 'name';")
+        interests = []
+        num_interests = 0
+        for record in cur:
+            interests.append(convert_interest_format(record[0]))
+            num_interests += 1
+
+
+        # Fill in data about interests from 'interests' table.
+        cur.execute("SELECT * FROM interests;")
+        for record in cur:
+            course_interests = []
+            for i in range(1, num_interests + 1):
+                if record[i]:
+                    course_interests.append(interests[i - 1])
+            classes_by_name[record[0]].interests = course_interests
+
+        for course in classes_by_name:
+            classes_offered.append(classes_by_name[course])
+
+
+
+def find_prereq_string(query_course):
+    solution = ""
+    conn = psycopg2.connect(host="localhost", database="Math Prerequisite Testing String", user="postgres",
+                            password="MN~D=bp~+WR2/ppy")
+    cur = conn.cursor()
+
+    cur.execute("SELECT prerequisites FROM courses WHERE name = " + query_course + ";")
+
+    unformatted_prereqs = cur.fetchone()[0]
+    if unformatted_prereqs == None:
+        return solution
+
+    split_prereqs = unformatted_prereqs.split()
+    #print(split_prereqs)
+
+    for word in split_prereqs:
+        if word not in ['or', 'and']: # If word is a classname and not a logical operator
+            if word[0] != '(':
+                new_word = "self.has_taken(" + word + ")"
+            else:
+                new_word = "(self.has_taken(" + word + ")"
+            if word[len(word) - 1] == ')':
+                new_word += ')'
+            solution += new_word + " "
+
+        else:
+            solution += word + " "
+    print(solution)
+    return solution
+
+if __name__ == "__main__":
+    classes_offered = []
+    classes_by_name = {}
+    get_info_from_database(classes_offered, classes_by_name)
+
+    test_classes_taken = {"MAT135A": classes_by_name["MAT135A"], "MAT108": classes_by_name["MAT108"], "MAT22A": classes_by_name["MAT22A"], "MAT67": classes_by_name["MAT67"]}
+    test_student = Student(major="LMOR", classes_taken=test_classes_taken)
+
+
+    query_course = "\'MAT128A\'"
+    prereqs = find_prereq_string(query_course)
+    #print(eval(prereqs))
+
