fa77d45b8012c8be7fcdfd76f03feeee1d63a9eb
gbrusman <gabrielbrusman@gmail.com>
2020-02-27 14:00:08 -0800

    Added path option to psycopg2 (not sure if entirely necessary).

--- make-windows.ps1
+++ make-windows.ps1
@@ -5,7 +5,7 @@
 
 $scripts = "AcademicTime.py", "AppliedSeriesChoicePage.py", "Course.py", "CourseSelectPage.py", "InterestSelectPage.py", "MajorSelectPage.py", "MultiPageApp.py", "ScheduleBlock.py", "ScheduleDisplayPage.py", "Schedule.py", "Student.py"
 
-$options = "-n", "ScheduleBotWINDOWS"
+$options = "-p", "C:\Users\gabri\PycharmProjects\ScheduleBotPy\venv\Lib\site-packages", "-n", "ScheduleBotWINDOWS", "--debug", "all"
 
 $spec = "ScheduleBotWINDOWS.spec"
 
